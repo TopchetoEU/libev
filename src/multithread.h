9df01344f66f4ddcf0010db1282e5490885ff434
TopchetoEU <36534413+TopchetoEU@users.noreply.github.com>
2026-01-11 01:27:53 +0200

    add timed cond waits for threading

--- src/multithread.h
+++ src/multithread.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "ev/conf.h"
+#include "ev.h"
+#include <assert.h>
 
 #ifdef EV_USE_WIN32
 	#include <winsock2.h>
@@ -21,12 +23,19 @@
 	#define ev_cond_new(cond) (InitializeConditionVariable(cond), 0)
 	// #define ev_cond_free(cond) (DeleteConditionVariable(cond), 0)
 	#define ev_cond_free(cond) ((void)cond)
-	#define ev_cond_wait(cond, mut) (SleepConditionVariableCS(cond, mut, INFINITE) ? 0 : -1)
+	#define ev_cond_wait(cond, mut) (void)SleepConditionVariableCS(cond, mut, INFINITE)
+	static inline int ev_cond_timewait(ev_cond_t cond, ev_mutex_t mut, ev_time_t timeout) {
+		ev_time_t curr;
+		ev_monotime(&curr);
+
+		int64_t ms = ev_timems(ev_timesub(timeout, curr));
+		if (ms < 0) ms = 0;
+
+		if (!SleepConditionVariableCS(cond, mut, ms)) return ETIMEDOUT;
+		return 0;
+	}
 	#define ev_cond_broadcast(cond) (WakeAllConditionVariable(cond), 0)
 	#define ev_cond_signal(cond) (WakeConditionVariable(cond), 0)
-
-
-
 #elif defined EV_USE_PTHREAD
 	#include <pthread.h>
 
@@ -42,16 +51,25 @@
 		return ret;
 	}
 
-	#define ev_mutex_new(mut) pthread_mutex_init(mut, (const pthread_mutexattr_t *)NULL)
-	#define ev_mutex_free(mut) pthread_mutex_destroy(mut)
-	#define ev_mutex_lock(mut) pthread_mutex_lock(mut)
-	#define ev_mutex_unlock(mut) pthread_mutex_unlock(mut)
+	#define ev_mutex_new(mut) (void)pthread_mutex_init(mut, (const pthread_mutexattr_t *)NULL)
+	#define ev_mutex_free(mut) (void)pthread_mutex_destroy(mut)
+	#define ev_mutex_lock(mut) (void)pthread_mutex_lock(mut)
+	#define ev_mutex_unlock(mut) (void)pthread_mutex_unlock(mut)
 
-	#define ev_cond_new(cond) pthread_cond_init(cond, (const pthread_condattr_t *)NULL)
+	static inline void ev_cond_new(ev_cond_t cond) {
+		pthread_condattr_t attr[1];
+		pthread_condattr_init(attr);
+		assert(pthread_condattr_setclock(attr, CLOCK_MONOTONIC) == 0);
+		pthread_cond_init(cond, attr);
+		pthread_condattr_destroy(attr);
+	}
 	#define ev_cond_free(cond) pthread_cond_destroy(cond)
-	#define ev_cond_signal(cond) pthread_cond_signal(cond)
-	#define ev_cond_broadcast(cond) pthread_cond_signal(cond)
-	#define ev_cond_wait(cond, mut) pthread_cond_wait(cond, mut)
+	#define ev_cond_signal(cond) (void)pthread_cond_signal(cond)
+	#define ev_cond_broadcast(cond) (void)pthread_cond_signal(cond)
+	#define ev_cond_wait(cond, mut) (void)pthread_cond_wait(cond, mut)
+	static inline int ev_cond_timewait(ev_cond_t cond, ev_mutex_t mut, ev_time_t timeout) {
+		return pthread_cond_timedwait(cond, mut, &(struct timespec) { .tv_sec = timeout.sec, .tv_nsec = timeout.nsec });
+	}
 #else
 	typedef struct {} pthread_mutex_t[1];
 
