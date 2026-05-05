692b7868e23953f0ea4f427538274cc34b12290c
TopchetoEU <36534413+TopchetoEU@users.noreply.github.com>
2026-05-05 12:45:15 +0300

    improve ev_strerr

--- inc/ev/errno.h
+++ inc/ev/errno.h
@@ -3,99 +3,104 @@
 
 // These have more or less been ripped from libuv
 
+#define EV_SIGDEF(X) \
+	X(OK, 0, "no error occurred") \
+	X(EPERM, -1, "operation not permitted") \
+	X(ENOENT, -2, "no such file or directory") \
+	X(ESRCH, -3, "no such process") \
+	X(EINTR, -4, "interrupted system call") \
+	X(EIO, -5, "i/o error") \
+	X(ENXIO, -6, "no such device or address") \
+	X(E2BIG, -7, "argument list too long") \
+	X(ENOEXEC, -8, "exec format error") \
+	X(EBADF, -9, "bad file descriptor") \
+	X(ECHILD, -10, "no child processes") \
+	X(EAGAIN, -11, "resource temporarily unavailable") \
+	X(ENOMEM, -12, "not enough memory") \
+	X(EACCES, -13, "permission denied") \
+	X(EFAULT, -14, "bad address in system call argument") \
+	X(ENOTBLK, -15, "Block device required") \
+	X(EBUSY, -16, "resource busy or locked") \
+	X(EEXIST, -17, "file already exists") \
+	X(EXDEV, -18, "cross-device link not permitted") \
+	X(ENODEV, -19, "no such device") \
+	X(ENOTDIR, -20, "not a directory") \
+	X(EISDIR, -21, "illegal operation on a directory") \
+	X(EINVAL, -22, "invalid argument") \
+	X(ENFILE, -23, "file table overflow") \
+	X(EMFILE, -24, "too many open files") \
+	X(ENOTTY, -25, "inappropriate ioctl for device") \
+	X(ETXTBSY, -26, "text file is busy") \
+	X(EFBIG, -27, "file too large") \
+	X(ENOSPC, -28, "no space left on device") \
+	X(ESPIPE, -29, "invalid seek") \
+	X(EROFS, -30, "read-only file system") \
+	X(EMLINK, -31, "too many links") \
+	X(EPIPE, -32, "broken pipe") \
+	X(EDOM, -33, "numerical argument out of domain") \
+	X(ERANGE, -34, "result too large") \
+	X(EDEADLK, -35, "resource deadlock avoided") \
+	X(ENAMETOOLONG, -36, "name too long") \
+	X(ENOLCK, -37, "no locks available") \
+	X(ENOSYS, -38, "function not implemented") \
+	X(ENOTEMPTY, -39, "directory not empty") \
+	X(ELOOP, -40, "too many symbolic links encountered") \
+	X(EUNATCH, -49, "protocol driver not attached") \
+	X(ENODATA, -61, "no data available") \
+	X(ENONET, -64, "machine is not on the network") \
+	X(ECOMM, -70, "communication error on send") \
+	X(EPROTO, -71, "protocol error") \
+	X(EOVERFLOW, -75, "value too large for defined data type") \
+	X(ENOTUNIQ, -76, "Name not unique on network") \
+	X(ELIBBAD, -80, "accessing a corrupted shared library") \
+	X(EILSEQ, -84, "illegal byte sequence") \
+	X(ENOTSOCK, -88, "socket operation on non-socket") \
+	X(EDESTADDRREQ, -89, "destination address required") \
+	X(EMSGSIZE, -90, "message too long") \
+	X(EPROTOTYPE, -91, "protocol wrong type for socket") \
+	X(ENOPROTOOPT, -92, "protocol not available") \
+	X(EPROTONOSUPPORT, -93, "protocol not supported") \
+	X(ESOCKTNOSUPPORT, -94, "socket type not supported") \
+	X(ENOTSUP, -95, "operation not supported") \
+	X(EPFNOSUPPORT, -96, "operation not supported on socket") \
+	X(EAFNOSUPPORT, -97, "address family not supported") \
+	X(EADDRINUSE, -98, "address already in use") \
+	X(EADDRNOTAVAIL, -99, "address not available") \
+	X(ENETDOWN, -100, "network is down") \
+	X(ENETUNREACH, -101, "network is unreachable") \
+	X(ECONNABORTED, -103, "software caused connection abort") \
+	X(ECONNRESET, -104, "connection reset by peer") \
+	X(ENOBUFS, -105, "no buffer space available") \
+	X(EISCONN, -106, "socket is already connected") \
+	X(ENOTCONN, -107, "socket is not connected") \
+	X(ESHUTDOWN, -108, "cannot send after transport endpoint shutdown") \
+	X(ETIMEDOUT, -110, "connection timed out") \
+	X(ECONNREFUSED, -111, "connection refused") \
+	X(EHOSTDOWN, -112, "host is down") \
+	X(EHOSTUNREACH, -113, "host is unreachable") \
+	X(EALREADY, -114, "connection already in progress") \
+	X(EREMOTEIO, -121, "remote I/O error") \
+	X(ENOMEDIUM, -123, "no medium found") \
+	X(ECANCELED, -125, "operation canceled") \
+	X(EAI_BADFLAGS, -1001, "bad ai_flags value") \
+	X(EAI_NONAME, -1002, "unknown node or service") \
+	X(EAI_AGAIN, -1003, "temporary failure") \
+	X(EAI_FAIL, -1004, "permanent failure") \
+	X(EAI_NODATA, -1005, "no address") \
+	X(EAI_FAMILY, -1006, "ai_family not supported") \
+	X(EAI_SOCKTYPE, -1007, "socket type not supported") \
+	X(EAI_SERVICE, -1008, "service not available for socket type") \
+	X(EAI_ADDRFAMILY, -1009, "address family not supported") \
+	X(EAI_MEMORY, -1010, "out of memory") \
+	X(EAI_OVERFLOW, -1012, "argument buffer overflow") \
+	X(EAI_CANCELED, -1101, "request canceled") \
+	X(ECHARSET, -2001, "invalid Unicode character") \
+	X(EUNKNOWN, -3000, "unknown OS-specific error") \
+
 typedef enum {
-	EV_OK = 0,
-	EV_EPERM = -1, // operation not permitted
-	EV_ENOENT = -2, // no such file or directory
-	EV_ESRCH = -3, // no such process
-	EV_EINTR = -4, // interrupted system call
-	EV_EIO = -5, // i/o error
-	EV_ENXIO = -6, // no such device or address
-	EV_E2BIG = -7, // argument list too long
-	EV_ENOEXEC = -8, // exec format error
-	EV_EBADF = -9, // bad file descriptor
-	EV_ECHILD = -10, // no child processes
-	EV_EAGAIN = -11, // resource temporarily unavailable
-	EV_ENOMEM = -12, // not enough memory
-	EV_EACCES = -13, // permission denied
-	EV_EFAULT = -14, // bad address in system call argument
-	EV_ENOTBLK = -15, // Block device required
-	EV_EBUSY = -16, // resource busy or locked
-	EV_EEXIST = -17, // file already exists
-	EV_EXDEV = -18, // cross-device link not permitted
-	EV_ENODEV = -19, // no such device
-	EV_ENOTDIR = -20, // not a directory
-	EV_EISDIR = -21, // illegal operation on a directory
-	EV_EINVAL = -22, // invalid argument
-	EV_ENFILE = -23, // file table overflow
-	EV_EMFILE = -24, // too many open files
-	EV_ENOTTY = -25, // inappropriate ioctl for device
-	EV_ETXTBSY = -26, // text file is busy
-	EV_EFBIG = -27, // file too large
-	EV_ENOSPC = -28, // no space left on device
-	EV_ESPIPE = -29, // invalid seek
-	EV_EROFS = -30, // read-only file system
-	EV_EMLINK = -31, // too many links
-	EV_EPIPE = -32, // broken pipe
-	EV_EDOM = -33, // numerical argument out of domain
-	EV_ERANGE = -34, // result too large
-	EV_EDEADLK = -35, // resource deadlock avoided
-	EV_ENAMETOOLONG = -36, // name too long
-	EV_ENOLCK = -37, // no locks available
-	EV_ENOSYS = -38, // function not implemented
-	EV_ENOTEMPTY = -39, // directory not empty
-	EV_ELOOP = -40, // too many symbolic links encountered
-	EV_EUNATCH = -49, // protocol driver not attached
-	EV_ENODATA = -61, // no data available
-	EV_ENONET = -64, // machine is not on the network
-	EV_ECOMM = -70, // communication error on send
-	EV_EPROTO = -71, // protocol error
-	EV_EOVERFLOW = -75, // value too large for defined data type
-	EV_ENOTUNIQ = -76, // Name not unique on network
-	EV_ELIBBAD = -80, // accessing a corrupted shared library
-	EV_EILSEQ = -84, // illegal byte sequence
-	EV_ENOTSOCK = -88, // socket operation on non-socket
-	EV_EDESTADDRREQ = -89, // destination address required
-	EV_EMSGSIZE = -90, // message too long
-	EV_EPROTOTYPE = -91, // protocol wrong type for socket
-	EV_ENOPROTOOPT = -92, // protocol not available
-	EV_EPROTONOSUPPORT = -93, // protocol not supported
-	EV_ESOCKTNOSUPPORT = -94, // socket type not supported
-	EV_ENOTSUP = -95, // operation not supported
-	EV_EPFNOSUPPORT = -96, // operation not supported on socket
-	EV_EAFNOSUPPORT = -97, // address family not supported
-	EV_EADDRINUSE = -98, // address already in use
-	EV_EADDRNOTAVAIL = -99, // address not available
-	EV_ENETDOWN = -100, // network is down
-	EV_ENETUNREACH = -101, // network is unreachable
-	EV_ECONNABORTED = -103, // software caused connection abort
-	EV_ECONNRESET = -104, // connection reset by peer
-	EV_ENOBUFS = -105, // no buffer space available
-	EV_EISCONN = -106, // socket is already connected
-	EV_ENOTCONN = -107, // socket is not connected
-	EV_ESHUTDOWN = -108, // cannot send after transport endpoint shutdown
-	EV_ETIMEDOUT = -110, // connection timed out
-	EV_ECONNREFUSED = -111, // connection refused
-	EV_EHOSTDOWN = -112, // host is down
-	EV_EHOSTUNREACH = -113, // host is unreachable
-	EV_EALREADY = -114, // connection already in progress
-	EV_EREMOTEIO = -121, // remote I/O error
-	EV_ENOMEDIUM = -123, // no medium found
-	EV_ECANCELED = -125, // operation canceled
-	EV_EAI_BADFLAGS = -1001, // bad ai_flags value
-	EV_EAI_NONAME = -1002, // unknown node or service
-	EV_EAI_AGAIN = -1003, // temporary failure
-	EV_EAI_FAIL = -1004, // permanent failure
-	EV_EAI_NODATA = -1005, // no address
-	EV_EAI_FAMILY = -1006, // ai_family not supported
-	EV_EAI_SOCKTYPE = -1007, // socket type not supported
-	EV_EAI_SERVICE = -1008, // service not available for socket type
-	EV_EAI_ADDRFAMILY = -1009, // address family not supported
-	EV_EAI_MEMORY = -1010, // out of memory
-	EV_EAI_OVERFLOW = -1012, // argument buffer overflow
-	EV_EAI_CANCELED = -1101, // request canceled
-	EV_ECHARSET = -2001, // invalid Unicode character
-	EV_EUNKNOWN = -3000, // unknown OS-specific error
+	#define EV_SIGDEF_ENUM_X(name, code, msg) EV_##name = code,
+	EV_SIGDEF(EV_SIGDEF_ENUM_X)
+	#undef EV_SIGDEF_ENUM_X
 } ev_code_t;
 
 #endif
